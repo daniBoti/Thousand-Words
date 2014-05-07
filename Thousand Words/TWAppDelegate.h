71809ef21f8894a2ec2fd69c004f9e7d646ca01d
Dani Boti <dani.fa82@gmail.com>
2014-05-07 16:41:13 +0200

    Initial Commit

---
+++ Thousand Words/TWAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  TWAppDelegate.h
+//  Thousand Words
+//
+//  Created by Dani on 07/05/14.
+//  Copyright (c) 2014 Dani Boti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ Thousand Words/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Thousand Words
+//
+//  Created by Dani on 07/05/14.
+//  Copyright (c) 2014 Dani Boti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TWAppDelegate class]));
+    }
+}
