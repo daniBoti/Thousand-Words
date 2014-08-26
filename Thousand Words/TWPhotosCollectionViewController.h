8400eb1121292f84d1303ac5757d3567cbd95f0e
Dani Boti <dani.fa82@gmail.com>
2014-08-27 00:32:44 +0200

    Add Class TWCollectionVC. Click on every album to acces a collection

---
+++ Thousand Words/TWPhotosCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  TWPhotosCollectionViewController.h
+//  Thousand Words
+//
+//  Created by Dani on 27/08/14.
+//  Copyright (c) 2014 Dani Boti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TWPhotosCollectionViewController : UICollectionViewController
+
+@end
---
+++ Thousand Words/TWPhotosCollectionViewController.m
@@ -0,0 +1,67 @@
+//
+//  TWPhotosCollectionViewController.m
+//  Thousand Words
+//
+//  Created by Dani on 27/08/14.
+//  Copyright (c) 2014 Dani Boti. All rights reserved.
+//
+
+#import "TWPhotosCollectionViewController.h"
+
+@interface TWPhotosCollectionViewController ()
+
+@end
+
+@implementation TWPhotosCollectionViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - UICollectionViewDataSource
+
+-(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Photo Cell";
+    
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:CellIdentifier forIndexPath:indexPath];
+    cell.backgroundColor = [UIColor whiteColor];
+    
+    return cell;
+}
+
+-(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return 5;
+}
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
